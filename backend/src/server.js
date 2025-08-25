593980cc243e7c0111847ff7ae7f935a53e0d2bd
fenohasina <fenohasinaherimamy@gmail.com>
2025-08-25 15:51:31 +0300

    envoi de server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,8 +9,7 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 
 
-const app = express();
-app.use(cors());
+const app = express();app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
