a978f416064b8517f52933bab487163bd9ec46ca
fenohasina <fenohasinaherimamy@gmail.com>
2025-08-26 09:49:54 +0300

    envoi de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,5 @@
  import Signin from "./Components/Signin"
-<<<<<<< HEAD
-const App = () => {
-    return (
-        <> 
-        <Signin></Signin>
-        </>
-        </>
-=======
-        <div>
-                
-        </div>
->>>>>>> f915923 (Auth system finished and ready for hand)
-    )
-}
-=======
+ 
 
 const App = () => {
     return (
@@ -23,5 +9,5 @@ const App = () => {
     )
 }
 
->>>>>>> b3ff610 (send the signin page all front without backend)
+ 
 export default App
