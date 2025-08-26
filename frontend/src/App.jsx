6c37b41bbcf004d920c392de7eb150a351164378
fenohasina <fenohasinaherimamy@gmail.com>
2025-08-26 10:46:55 +0300

    mettre la page par défaut

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+ import { Routes, Route, Navigate } from 'react-router-dom';
 import Register from './Components/Auth/Register';
-import Login from './Components/Auth/Login';
 import Home from './pages/Home';
+import Signin from './Components/Auth/Signin';
 
 function App() {
   return (
     <Routes>
+      {/* Route par défaut : redirige "/" vers "/login" */}
+      <Route path="/" element={<Navigate to="/Signin" replace />} />
+
       <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/Signin" element={<Signin />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
--- frontend/src/Components/Auth/Signin.jsx
+++ frontend/src/Components/Auth/Signin.jsx
@@ -1,4 +1,4 @@
- 
+
 const Signin = () => {
   const LeftPanel = () => {
     return (
