cf7c3e09ff92fa9e25d243e87bebaa121a0e1605
fenohasina <fenohasinaherimamy@gmail.com>
2025-09-11 14:37:35 +0300

    total of amount

--- frontend/src/Components/Site/Incomes.jsx
+++ frontend/src/Components/Site/Incomes.jsx
@@ -1,13 +1,13 @@
+
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import ContentIncomes from "./ContentIncomes";
- 
 
 const Incomes = () => {
-    return (
-     <div className="flex flex-col h-screen w-full bg-gray-100">
+  return (
+    <div className="flex flex-col h-screen w-full bg-gray-100">
       {/* Header fixed on top */}
       <Header className="flex-none" />
 
@@ -19,11 +19,11 @@ const Incomes = () => {
         </div>
 
         {/* Content takes remaining space, scrollable */}
-        <div className="flex-1 h-full overflow-auto">
+        <div className="flex-1 h-full overflow-auto p-2">
           <ContentIncomes />
         </div>
       </div>
     </div>
   );
-}
-export default Incomes 
+};
+export default Incomes;
