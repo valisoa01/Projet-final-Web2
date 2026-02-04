2ad5716fb54f9225e60b86ff3fd63c6dfe2cc7c4
valisoa01 <valisoatolotriniaina@gmail.com>
2026-02-04 21:23:20 +0300

    add all

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({ 
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://projet-final-web2.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
